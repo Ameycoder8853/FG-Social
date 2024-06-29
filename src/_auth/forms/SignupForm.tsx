a6e1a095845bcb7a1e9544b6aca66a34f60cd273
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-06-29 13:47:11 +0530

    Update SignupForm.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -81,7 +81,7 @@ const SignupForm = () => {
           Create a new account
         </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use snapgram, Please enter your details
+          To use FG Social, Please enter your details
         </p>
 
         <form
